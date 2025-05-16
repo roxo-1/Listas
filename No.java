774840a8e19968ef51fad343e0e390757aa2f05e
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-05-16 00:20:48 +0000

    Nó

--- No.java
+++ No.java
@@ -1,3 +1,11 @@
 public class Node <T>{
-    //pass
+    T modulo; //valor genérico
+    private Node proximo; // ponteiro apontando para o anterior
+    private Node anterior; //ponteiro apontado para o próximo
+
+    public Node (T modulo){
+        this.modulo = modulo;
+        this.proximo = null;
+        this.anterior = null;
+    }
 }
