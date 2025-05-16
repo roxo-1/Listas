4007ccae006386bc63fb1a32b30777a13fec5089
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-05-16 00:43:32 +0000

    Update operações básicas lista

--- ListaDuplamenteEncadeada.java
+++ ListaDuplamenteEncadeada.java
@@ -1,3 +1,29 @@
 public class ListaDuplamenteEncadeada<T>{
-// erro aqui: falta abrir o corpo da classe
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public ListaDuplamenteEncadeada(){
+        this.head = null;
+        this.tail = null;
+        size = 0;
+    }
+
+    // OPERAÇÃO: size() Retorna a quantidade de nós da lista.
+    public int size() {
+        return size;
+    }
+    // OPERAÇÃO: getHead() Retorna uma referência para o nó do início da lista, caso a lista estiver vazia retorna null.
+	public Node<T> getHead() {
+		return head;
+	}
+
+    // OPERAÇÃO: getTail() Retorna uma referência para o nó do final da lista, caso a lista estiver vazia retorna null.
+	public Node<T> getTail() {
+		return tail;
+	}
+    // OPERAÇÃO: isEmpty() Retorna uma referência para o nó do final da lista, caso a lista estiver vazia retorna null.
+	public boolean isEmpty() {
+    return head == null;
+    }
 }
